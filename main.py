9f01f62b8f58136d6ea936ed13913c97d10a19ad
michael-zenge <michael.zenge@gmail.com>
2022-11-18 16:12:33 +0100

    Nudging snake towards fruit, intermediate version.

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 def on_up_pressed():
-    doAction(4)
+    doAction(3)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
 def initQTable():
     global lQ
     lQ = []
     for index in range(1024):
-        lQ.append([0, 0, 0, 0, 0])
+        lQ.append([0, 0, 0, 0])
 
 def on_a_pressed():
     game.over(True)
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
-def getReward(stateIdx: number):
+def getReward(stateIdx: number, actionIdx: number):
     if stateIdx >= 512:
-        return -1
+        return -10
     elif stateIdx >= 256:
-        return 1
-    return 0
+        return 10
+    else:
+        if snakeHead.tilemap_location().column > snakeFood.tilemap_location().column and snakeHead.tilemap_location().row > snakeFood.tilemap_location().row:
+            if actionIdx == 3 or actionIdx == 1:
+                return 1
+        elif snakeHead.tilemap_location().column < snakeFood.tilemap_location().column and snakeHead.tilemap_location().row > snakeFood.tilemap_location().row:
+            if actionIdx == 3 or actionIdx == 0:
+                return 1
+        elif snakeHead.tilemap_location().column < snakeFood.tilemap_location().column and snakeHead.tilemap_location().row < snakeFood.tilemap_location().row:
+            if actionIdx == 2 or actionIdx == 0:
+                return 1
+        elif snakeHead.tilemap_location().column > snakeFood.tilemap_location().column and snakeHead.tilemap_location().row < snakeFood.tilemap_location().row:
+            if actionIdx == 2 or actionIdx == 1:
+                return 1
+        return -1
 
 def on_left_pressed():
-    doAction(2)
+    doAction(1)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
 def eatFood():
-    global tmpLocation
     if tiles.tile_at_location_equals(snakeHead.tilemap_location(),
         assets.tile("""
             myTile1
@@ -34,44 +46,47 @@ def eatFood():
                 myTile0
             """))
         while True:
-            tmpLocation = tiles.get_tile_location(randint(2, 11), randint(2, 9))
-            if not (tiles.tile_at_location_equals(tmpLocation, assets.tile("""
-                myTile
-            """))):
-                tiles.set_tile_at(tmpLocation, assets.tile("""
-                    myTile1
-                """))
+            tiles.place_on_tile(snakeFood,
+                tiles.get_tile_location(randint(2, 11), randint(2, 9)))
+            if not (tiles.tile_at_location_equals(snakeFood.tilemap_location(),
+                assets.tile("""
+                    myTile
+                """))):
+                tiles.set_tile_at(snakeFood.tilemap_location(),
+                    assets.tile("""
+                        myTile1
+                    """))
                 break
         listSnake.unshift(snakeHead.tilemap_location())
         info.change_score_by(1)
 
 def on_right_pressed():
-    doAction(1)
+    doAction(0)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
-def getState(colOffset: number, rowOffset: number, basis: number):
+def getState(colOffset: number, rowOffset: number, base: number):
     if tiles.tile_at_location_equals(tiles.get_tile_location(snakeHead.tilemap_location().column + colOffset,
             snakeHead.tilemap_location().row + rowOffset),
         assets.tile("""
             myTile1
         """)):
-        return basis
+        return base
     elif tiles.tile_at_location_equals(tiles.get_tile_location(snakeHead.tilemap_location().column + colOffset,
             snakeHead.tilemap_location().row + rowOffset),
         assets.tile("""
             myTile
         """)):
-        return basis * 2
+        return base * 2
     elif tiles.tile_at_location_equals(tiles.get_tile_location(snakeHead.tilemap_location().column + colOffset,
             snakeHead.tilemap_location().row + rowOffset),
         assets.tile("""
             Wall
         """)):
-        return basis * 2 + basis
+        return base * 2 + base
     else:
         return 0
 def moveSnake():
-    index2 = 0
+    global index2
     while index2 <= len(listSnake) - 1:
         if index2 == 0:
             tiles.set_tile_at(listSnake[index2], assets.tile("""
@@ -89,11 +104,12 @@ def moveSnake():
         index2 += 1
 def resetSnake():
     global listSnake
-    info.set_score(0)
     tiles.set_current_tilemap(tilemap("""
         level1
     """))
-    tiles.set_tile_at(tiles.get_tile_location(randint(2, 11), randint(2, 9)),
+    tiles.place_on_tile(snakeFood,
+        tiles.get_tile_location(randint(2, 11), randint(2, 9)))
+    tiles.set_tile_at(snakeFood.tilemap_location(),
         assets.tile("""
             myTile1
         """))
@@ -101,7 +117,7 @@ def resetSnake():
     listSnake.unshift(tiles.get_tile_location(4, 4))
     tiles.place_on_tile(snakeHead, listSnake[0])
     listSnake.unshift(snakeHead.tilemap_location())
-    doAction(3)
+    doAction(2)
 def checkCollision():
     if tiles.tile_at_location_equals(snakeHead.tilemap_location(), assets.tile("""
         Wall
@@ -109,36 +125,36 @@ def checkCollision():
         assets.tile("""
             myTile
         """)):
-        pause(2000)
+        info.change_score_by(1)
+        pause(500)
         resetSnake()
 
 def on_down_pressed():
-    doAction(3)
+    doAction(2)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 def getStateIdx():
     return getState(0, 0, 256) + (getState(-1, 0, 64) + (getState(0, 1, 16) + (getState(1, 0, 4) + getState(0, -1, 1))))
 def doAction(Idx: number):
     global colInc, rowInc
-    if Idx == 4:
+    if Idx == 3:
         colInc = 0
         rowInc = -1
         return Idx
-    elif Idx == 3:
+    elif Idx == 2:
         colInc = 0
         rowInc = 1
         return Idx
-    elif Idx == 2:
+    elif Idx == 1:
         colInc = -1
         rowInc = 0
         return Idx
-    elif Idx == 1:
+    elif Idx == 0:
         colInc = 1
         rowInc = 0
         return Idx
     else:
-        return Idx
-    return -1
+        return -1
 def doQAction(StateIdx: number):
     global iMaxIdx
     iMaxIdx = 0
@@ -146,19 +162,40 @@ def doQAction(StateIdx: number):
         if lQ[StateIdx][index3] > lQ[StateIdx][iMaxIdx]:
             iMaxIdx = index3
     return doAction(iMaxIdx)
-def doQUpdate(stateIdx2: number, actionIdx: number, reward: number, alpha: number):
-    lQ[stateIdx2][actionIdx] = (1 - alpha) * lQ[stateIdx2][actionIdx] + alpha * reward
+def doQUpdate(stateIdx2: number, actionIdx2: number, alpha: number):
+    lQ[stateIdx2][actionIdx2] = (1 - alpha) * lQ[stateIdx2][actionIdx2] + alpha * getReward(getStateIdx(), actionIdx2)
     print(lQ[stateIdx2])
 currActionIdx = 0
 currStateIdx = 0
 iMaxIdx = 0
 rowInc = 0
 colInc = 0
+index2 = 0
 listSnake: List[tiles.Location] = []
-tmpLocation: tiles.Location = None
 lQ: List[List[number]] = []
 snakeHead: Sprite = None
+snakeFood: Sprite = None
+info.set_score(0)
 initQTable()
+snakeFood = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f . f . . . . . . . 
+            . . . . . . . f . . . . . . . . 
+            . . . . . . f . f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.food)
 snakeHead = sprites.create(img("""
         . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
             . 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
@@ -185,12 +222,12 @@ def on_update_interval():
     global currStateIdx, currActionIdx
     currStateIdx = getStateIdx()
     if randint(0, 100) < 20:
-        currActionIdx = doAction(randint(0, 4))
+        currActionIdx = doAction(randint(0, 3))
     else:
         currActionIdx = doQAction(currStateIdx)
     checkCollision()
     eatFood()
     moveSnake()
-    snakeHead.say_text("" + str(currStateIdx) + "," + str(currActionIdx))
-    doQUpdate(currStateIdx, currActionIdx, getReward(getStateIdx()), 0.2)
+    snakeHead.say_text(getReward(getStateIdx(), currActionIdx))
+    doQUpdate(currStateIdx, currActionIdx, 0.2)
 game.on_update_interval(350, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    doAction(4)
+    doAction(3)
 })
 function initQTable () {
     lQ = []
     for (let index = 0; index < 1024; index++) {
         lQ.push([
-        0,
         0,
         0,
         0,
@@ -16,24 +15,42 @@ function initQTable () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     game.over(true)
 })
-function getReward (stateIdx: number) {
+function getReward (stateIdx: number, actionIdx: number) {
     if (stateIdx >= 512) {
-        return -1
+        return -10
     } else if (stateIdx >= 256) {
-        return 1
+        return 10
+    } else {
+        if (snakeHead.tilemapLocation().column > snakeFood.tilemapLocation().column && snakeHead.tilemapLocation().row > snakeFood.tilemapLocation().row) {
+            if (actionIdx == 3 || actionIdx == 1) {
+                return 1
+            }
+        } else if (snakeHead.tilemapLocation().column < snakeFood.tilemapLocation().column && snakeHead.tilemapLocation().row > snakeFood.tilemapLocation().row) {
+            if (actionIdx == 3 || actionIdx == 0) {
+                return 1
+            }
+        } else if (snakeHead.tilemapLocation().column < snakeFood.tilemapLocation().column && snakeHead.tilemapLocation().row < snakeFood.tilemapLocation().row) {
+            if (actionIdx == 2 || actionIdx == 0) {
+                return 1
+            }
+        } else if (snakeHead.tilemapLocation().column > snakeFood.tilemapLocation().column && snakeHead.tilemapLocation().row < snakeFood.tilemapLocation().row) {
+            if (actionIdx == 2 || actionIdx == 1) {
+                return 1
+            }
+        }
+        return -1
     }
-    return 0
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    doAction(2)
+    doAction(1)
 })
 function eatFood () {
     if (tiles.tileAtLocationEquals(snakeHead.tilemapLocation(), assets.tile`myTile1`)) {
         tiles.setTileAt(snakeHead.tilemapLocation(), assets.tile`myTile0`)
         while (true) {
-            tmpLocation = tiles.getTileLocation(randint(2, 11), randint(2, 9))
-            if (!(tiles.tileAtLocationEquals(tmpLocation, assets.tile`myTile`))) {
-                tiles.setTileAt(tmpLocation, assets.tile`myTile1`)
+            tiles.placeOnTile(snakeFood, tiles.getTileLocation(randint(2, 11), randint(2, 9)))
+            if (!(tiles.tileAtLocationEquals(snakeFood.tilemapLocation(), assets.tile`myTile`))) {
+                tiles.setTileAt(snakeFood.tilemapLocation(), assets.tile`myTile1`)
                 break;
             }
         }
@@ -42,21 +59,21 @@ function eatFood () {
     }
 }
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    doAction(1)
+    doAction(0)
 })
-function getState (colOffset: number, rowOffset: number, basis: number) {
+function getState (colOffset: number, rowOffset: number, base: number) {
     if (tiles.tileAtLocationEquals(tiles.getTileLocation(snakeHead.tilemapLocation().column + colOffset, snakeHead.tilemapLocation().row + rowOffset), assets.tile`myTile1`)) {
-        return basis
+        return base
     } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(snakeHead.tilemapLocation().column + colOffset, snakeHead.tilemapLocation().row + rowOffset), assets.tile`myTile`)) {
-        return basis * 2
+        return base * 2
     } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(snakeHead.tilemapLocation().column + colOffset, snakeHead.tilemapLocation().row + rowOffset), assets.tile`Wall`)) {
-        return basis * 2 + basis
+        return base * 2 + base
     } else {
         return 0
     }
 }
 function moveSnake () {
-    for (let index2 = 0; index2 <= listSnake.length - 1; index2++) {
+    while (index2 <= listSnake.length - 1) {
         if (index2 == 0) {
             tiles.setTileAt(listSnake[index2], assets.tile`myTile0`)
         }
@@ -67,51 +84,52 @@ function moveSnake () {
             listSnake[index2] = tiles.getTileLocation(listSnake[index2 + 1].column, listSnake[index2 + 1].row)
             tiles.setTileAt(listSnake[index2], assets.tile`myTile`)
         }
+        index2 += 1
     }
 }
 function resetSnake () {
-    info.setScore(0)
     tiles.setCurrentTilemap(tilemap`level1`)
-    tiles.setTileAt(tiles.getTileLocation(randint(2, 11), randint(2, 9)), assets.tile`myTile1`)
+    tiles.placeOnTile(snakeFood, tiles.getTileLocation(randint(2, 11), randint(2, 9)))
+    tiles.setTileAt(snakeFood.tilemapLocation(), assets.tile`myTile1`)
     listSnake = []
     listSnake.unshift(tiles.getTileLocation(4, 4))
     tiles.placeOnTile(snakeHead, listSnake[0])
     listSnake.unshift(snakeHead.tilemapLocation())
-    doAction(3)
+    doAction(2)
 }
 function checkCollision () {
     if (tiles.tileAtLocationEquals(snakeHead.tilemapLocation(), assets.tile`Wall`) || tiles.tileAtLocationEquals(snakeHead.tilemapLocation(), assets.tile`myTile`)) {
-        pause(2000)
+        info.changeScoreBy(1)
+        pause(500)
         resetSnake()
     }
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    doAction(3)
+    doAction(2)
 })
 function getStateIdx () {
     return getState(0, 0, 256) + (getState(-1, 0, 64) + (getState(0, 1, 16) + (getState(1, 0, 4) + getState(0, -1, 1))))
 }
 function doAction (Idx: number) {
-    if (Idx == 4) {
+    if (Idx == 3) {
         colInc = 0
         rowInc = -1
         return Idx
-    } else if (Idx == 3) {
+    } else if (Idx == 2) {
         colInc = 0
         rowInc = 1
         return Idx
-    } else if (Idx == 2) {
+    } else if (Idx == 1) {
         colInc = -1
         rowInc = 0
         return Idx
-    } else if (Idx == 1) {
+    } else if (Idx == 0) {
         colInc = 1
         rowInc = 0
         return Idx
     } else {
-        return Idx
+        return -1
     }
-    return -1
 }
 function doQAction (StateIdx: number) {
     iMaxIdx = 0
@@ -122,20 +140,40 @@ function doQAction (StateIdx: number) {
     }
     return doAction(iMaxIdx)
 }
-function doQUpdate (stateIdx: number, actionIdx: number, reward: number, alpha: number) {
-    lQ[stateIdx][actionIdx] = (1 - alpha) * lQ[stateIdx][actionIdx] + alpha * reward
-    console.log(lQ[stateIdx])
+function doQUpdate (stateIdx2: number, actionIdx2: number, alpha: number) {
+    lQ[stateIdx2][actionIdx2] = (1 - alpha) * lQ[stateIdx2][actionIdx2] + alpha * getReward(getStateIdx(), actionIdx2)
+    console.log(lQ[stateIdx2])
 }
 let currActionIdx = 0
 let currStateIdx = 0
 let iMaxIdx = 0
 let rowInc = 0
 let colInc = 0
+let index2 = 0
 let listSnake: tiles.Location[] = []
-let tmpLocation: tiles.Location = null
 let lQ: number[][] = []
 let snakeHead: Sprite = null
+let snakeFood: Sprite = null
+info.setScore(0)
 initQTable()
+snakeFood = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f . f . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . f . f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
 snakeHead = sprites.create(img`
     . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
     . 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
@@ -159,13 +197,13 @@ resetSnake()
 game.onUpdateInterval(350, function () {
     currStateIdx = getStateIdx()
     if (randint(0, 100) < 20) {
-        currActionIdx = doAction(randint(0, 4))
+        currActionIdx = doAction(randint(0, 3))
     } else {
         currActionIdx = doQAction(currStateIdx)
     }
     checkCollision()
     eatFood()
     moveSnake()
-    snakeHead.sayText("" + currStateIdx + "," + currActionIdx)
-    doQUpdate(currStateIdx, currActionIdx, getReward(getStateIdx()), 0.2)
+    snakeHead.sayText(getReward(getStateIdx(), currActionIdx))
+    doQUpdate(currStateIdx, currActionIdx, 0.2)
 })
